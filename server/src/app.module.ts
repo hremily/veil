62d37f642211b872462c3d2ce733c4bd7391a6e3
milunchik <emili061116@gmail.com>
2024-09-18 00:19:35 +0300

    set up environment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import {ConfigModule, ConfigService} from '@nestjs/config'
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      "mongodb+srv://milunchik:3MpK7aRInEfQFUZn@veilcluster.xw8xy.mongodb.net/?retryWrites=true&w=majority&appName=VeilCluster"
-    )
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          uri: `mongodb+srv://milunchik:${config.get<string>('DB_PASSWORD')}@${config.get<string>('DB_CLUSTER')}.xw8xy.mongodb.net/?retryWrites=true&w=majority&appName=VeilCluster`,
+        };
+      }
+    }),
+    
   ],
   controllers: [AppController],
   providers: [AppService],
