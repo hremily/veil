f3e2d4088761a245c63e5d49475d39f6e8d1e471
milunchik <emili061116@gmail.com>
2024-09-19 21:31:53 +0300

    added teacher settings

---
+++ server/src/teacher/teacher.schema.ts
@@ -0,0 +1,24 @@
+import { Schema } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+
+Schema()
+export const TeacherSchema = new mongoose.Schema({
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    fullname: {type: String, required: true, default: ''},
+    phone_number: {type: String, required: true, default: ''},
+    experience: {type: String, required: true, default: ''},
+    lessons: {type: String, required: true, default: ''},
+    desciption: {type :String, required: true, default: ''},
+    price: {type: Number, required: true, default: 0}
+})
+
+export type TeacherType = {
+    email: {type: String},
+    password: {type: String,},
+    fullname: {type: String,},
+    phone_number: {type: String,},
+    experience: {type: String, },
+    lessons: {type: String, },
+    desciption: {type :String, }
+}
--- server/src/user/user.schema.ts
+++ server/src/user/user.schema.ts
@@ -4,14 +4,17 @@ import mongoose from "mongoose";
 Schema()
 export const UserSchema = new mongoose.Schema({
     email: {type: String, unique: true, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true,},
+    fullname: {type: String, required: true,  default: ''},
+    phone_number: {type: String, required: true, default: ''}
 }
 )
 
 export type UserType = {
     name: {type: String},
-    password: {type :String}
-
+    password: {type :String},
+    fullname: {type:String},
+    phone_number: {type: String}
 }
 
 //for students
