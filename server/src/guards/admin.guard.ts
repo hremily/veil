09f02fc118326820ada0ba09776db6781fecc4c1
milunchik <emili061116@gmail.com>
2024-09-28 22:24:28 +0300

    change admin access

--- server/src/guards/admin.guard.ts
+++ server/src/guards/admin.guard.ts
@@ -3,10 +3,11 @@ import { CanActivate, ExecutionContext } from '@nestjs/common';
 export class AdminGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    if (!request.currentUser) {
+    if (!request.user) {
       return false;
     }
 
-    return request.currentUser.admin;
+    const userRole = request.session.userRole;
+    return userRole === 'admin';
   }
 }
