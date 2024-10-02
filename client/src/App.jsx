1fed03e83ae2f364ea9a00ec9a80c65d5f0ed340
milunchik <emili061116@gmail.com>
2024-10-02 23:20:50 +0300

    changed setup configurations

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import resizeHook from '@hooks/resize';
+
+import styles from './App.css';
+// import * as styles from './App.css';
+
+const App = () => {
+    return <div className={styles.main}>My first application</div>;
+};
+
+// export { App };
+export default App;
