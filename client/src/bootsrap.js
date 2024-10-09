f113454c365309b2547235447245619e83b59381
milunchik <emili061116@gmail.com>
2024-10-09 22:23:00 +0300

    bugfix

--- client/src/bootsrap.js
+++ client/src/bootsrap.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 // import { App } from './App';
 import App from './App';
 
 const domNode = document.getElementById('react-app');
+console.log('DOM Node:', domNode);
 const root = createRoot(domNode);
 
 root.render(<App />);
