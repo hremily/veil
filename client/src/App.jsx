b03b65588824f7a8caba771445fd1807ad5d6d47
milunchik <emili061116@gmail.com>
2024-10-13 02:31:37 +0300

    added header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,15 @@ import Footer from './components/Footer/Footer';
 import SignUpTeacher from './pages/SignPages/SignUpTeacher.jsx';
 import SignIn from './pages/SignPages/SignIn.jsx';
 import SignUpUser from './pages/SignPages/SignUpUser.jsx';
+import Header from './components/Header/Header.jsx';
 function App() {
     return (
         <>
-            <SignIn />
+        <Header />
+            {/* <SignIn />
             <SignUpTeacher />
             <SignUpUser />
-            <Footer />
+            <Footer /> */}
         </>
     );
 }
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import styles from './Header.module.css';
 
-function SignUp() {
+function Header() {
     return (
-        <div className="illustration-section">
-            <h1>
-                <img src="../images/VEIL_logo.png" alt="Illustration" id="logo" className="logo"></img>eil
-            </h1>
-        </div>
+        <header>
+            <div className={styles.wrapper}>
+                <div className={styles.illustrationSection}>
+                    <div className={styles.logoWrapper}>
+                        <img src="../images/VEIL_logo.png" alt="Logo" className={styles.logo} />
+                        <span className={styles.logoText}>eil {'|'} </span>
+                        <a href="home" className={styles.homeLink}>
+                            HOME
+                        </a>
+                    </div>
+                </div>
+                <nav className={styles.nav}>
+                    <a href="#categories">Categories</a>
+
+                    <a href="#profile">Profile</a>
+                    <a href="signout">
+                        <img src="../../images/logout.png" alt="signout" />
+                    </a>
+                </nav>
+            </div>
+        </header>
     );
 }
+
+export default Header;
