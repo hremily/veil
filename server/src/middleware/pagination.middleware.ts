c200f976a6fcdca940bbae1f7c10cb93ed9bffbf
milunchik <emili061116@gmail.com>
2024-10-17 23:47:49 +0300

    remove backend

--- server/src/middleware/pagination.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { DEFAULT_PAGE_SIZE, PAGE } from '../utils/pagination.constans';
-
-export const paginationFunc = (limit: number, skip: number) => {
-  const pageSize = limit || DEFAULT_PAGE_SIZE;
-  const calcSkip = (PAGE - 1) * pageSize;
-  const pageSkip = skip || calcSkip;
-
-  return { pageSize, pageSkip };
-};
