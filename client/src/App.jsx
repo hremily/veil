c5b3b52aadc90eea227c03f40abd2cd1bafcfd93
milunchik <emili061116@gmail.com>
2024-10-25 04:20:40 +0300

    created colors constans

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './App.css';
-
+import Questionary from './components/Questionary/Questionary';
 function App() {
     return (
         <>
+            <Questionary />
         </>
     );
 }
